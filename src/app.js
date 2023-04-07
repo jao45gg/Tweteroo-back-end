6f5cae9832cf1decc2df99f6ea9f3c595ea40910
joao <joaosb004@gmail.com>
2023-04-07 11:39:56 -0300

    Route POST '/tweets' done

--- src/app.js
+++ src/app.js
@@ -6,13 +6,23 @@ app.use(cors());
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
-app.post("/sign-up", (req,res) => {
+app.post("/sign-up", (req, res) => {
 
     const { username, avatar } = req.body;
-    users.push({username, avatar});
-    
+    users.push({ username, avatar });
+
     res.send("OK");
+
+});
+
+app.post("/tweets", (req, res) => {
+
+    const { username, tweet } = req.body;
+
+    const found = users.find(u => u.username === username);
+    found ? tweets.push({ username, tweet }) && res.send("OK") : res.send("UNAUTHORIZED");
 
 });
 
